98218deea5d5e992ed6ab20abcd94bad0ea70b52
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2023-12-20 21:12:12 -0300

    aprendendo sobre jsx

---
+++ index.jsx
@@ -0,0 +1,12 @@
+const element = <h1>Olá, mundo!</h1>
+
+const anotherElement = (
+    <div>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+)
