f30792821541853d7aaff515e67f97a29156fcc9
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2024-01-17 20:56:40 -0300

    usando props do react

--- moviecard/card/src/App.jsx
+++ moviecard/card/src/App.jsx
@@ -1,9 +1,21 @@
 import Card from "./components/Card"
-
-export default function App() {
+import d1PosterImg from "./assets/dorohedoro.png"
+import d2PosterImg from "./assets/dorohedoro2.png"
+import d3PosterImg from "./assets/dorohedoro3.png"
+import d4PosterImg from "./assets/dorohedoro4.png"
+import Button from "./components/Button"
+ function App() {
   return (
-    <div>
-      <Card/>
-    </div>
+    <>
+      <h1>Exercicio</h1>
+      <Button text="Voltar ao inicio"/>
+      <Card title="Pôster:Volume 1" posterImg={d1PosterImg}/>
+      <Card title="Pôster:Volume 2" posterImg={d2PosterImg}/>
+      <Card title="Poster:Volume 3" posterImg={d3PosterImg}/>
+      <Card title="Poster:Volume 4" posterImg={d4PosterImg}/>
+     
+    </>
   )
 }
+
+export default App
--- moviecard/card/src/components/Card/index.jsx
+++ moviecard/card/src/components/Card/index.jsx
@@ -1,14 +1,16 @@
-import posterImg from "../../assets/dorohedoro.png"
+
+import Button from "../Button"
 import styles from"./styles.module.css"
 
-export default function Card() {
+
+export default function Card({title, posterImg }) {
     return (
         <div className = {styles.container}>
-            <img className={styles.poster} src={posterImg} alt="Dorohedoro Poster"/>
+            <img className={styles.poster} src={posterImg} alt={title}/>
             <div>
-                <h2 className = {styles.title}>Poster do manga Dorohedoro</h2>
-                <p className = {styles.description}>Um pôster decorativo do manga de Dorohedoro, com Moldura de MDF e tamanho A3. Uma ótima recordação da história do manga. Não perca a chance de adicionar esta memória a seu acervo</p>
-                <button className={styles.button}>Comprar agora</button>
+                <h2 >{title}</h2>
+                <p>Um pôster decorativo do manga de Dorohedoro, com Moldura de MDF e tamanho A3. Uma ótima recordação da história do manga. Não perca a chance de adicionar esta memória a seu acervo</p>
+               <Button text= "Comprar agora"/>
                 </div>
                  </div>
     )
