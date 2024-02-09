3035b61cc52a90a2db1a52a82a5313d87ef0257e
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2024-02-08 21:26:47 -0300

    comentando o codigo

--- gerador-de-senhas/src/App.jsx
+++ gerador-de-senhas/src/App.jsx
@@ -1,26 +1,36 @@
+//Esta linha importa a função `useState´ do pacote "react". `useState`é um hook do React que permite adicionar o estado a componentes de função
 import {useState} from "react"
-
+//Declaração de um componente de função chamado `App`
 function App() {
+  //Declaração de dois estados usando o `useState`hook. `password`armazenará a senha gerada, inicializada como uma string vazia. `copyText`armazenará o texto do botão de cópia, inicializado como Copiar.
   const [password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copiar")
-
+  //Declaração de uma função chamada `generate`, responsável por gerar uma senha aleatória
   function generate() {
+    //Definição de caracteres que serão usados para gerar a senha
     const characters = "'123456789-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ}"
+    // Definição do comprimento da senha
     const length = 12
+    //Inicialização da variável `newPassword`que armazenará a nova senha
     let newPassword = ""
+    //Laço de repetição para iterar sobre o comprimento da senha
     for (let i = 0; i < length; i++){
+    //Gera um número aleatório entre 0 e o comprimento da string `characters`, representando a posição de um caractere aleatório
       const position = Math.floor(Math.random() * characters.length)
       newPassword += characters[position]
     }
+    //Atualiza o estado `password`com a nova senha gerada e o estado `copyText`com o texto "Copiar"
     setPassword(newPassword)
     setCopyText("Copiar")
   }
-
+   //Declaração de uma função chamada `copyToClipboard`, responsável por copiar a senha para a área de transferência
   function copyToClipboard() {
+    //Usa a API Clipboard para escrever o texto da senha na área de transferência
     window.navigator.clipboard.writeText(password)
+    //Atualiza o estado `copyText`com o texto "Copiado!" após a senha ser copiada com sucesso
     setCopyText("Copiado!")
   }
-
+    //Retorna o JSX que representa a interface do componente `App`. Inclui um cabeçalho, dois botões(um para gerar senha e outro para copiar senha) e um elemento `div` para exibir a senha gerada
  return(
    <div className="app">
      <h1>Gerador de senhas</h1>
--- gerador-de-senhas/src/main.jsx
+++ gerador-de-senhas/src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react'//Importa o módulo `ReactDom` da biblioteca `react-dom`. Este código especificamente importa o método ´createRoot` de `ReactDom`que permite criar uma raiz para a renderização de componentes React.
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+//Cria uma renderização dos componentes React no elemento com o id "root" no documento HTML.
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Renderiza o componente `App`dentro de `<React.StrictMode>`.`<React.StrixtMode>` é um componente especial do React que envolve o aplicativo e fornece verificação de práticas desatualizadas e desencorajadas. Isso ajuda a identificar problemas potenciais no código.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
